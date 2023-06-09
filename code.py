f4f51840106bda76312dbdaa4406a20a3b616d9c
Drupalalt <124355661+Drupalalt@users.noreply.github.com>
2023-06-09 22:45:19 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,76 +1,94 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+import logging
+import concurrent.futures
 
-def process_wiki_page(url):
-    # Retrieve the web page
-    response = requests.get(url)
-    html = response.content
-    
-    # Parse the HTML with BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    # Extract the page content
-    title = soup.find('h1', {'class': 'firstHeading'}).text
-    paragraphs = soup.find_all('p')
-    content = '\n'.join([p.text for p in paragraphs])
-    
-    # Process the content here
-    # ...
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    # Return the processed content
-    return {
-        'title': title,
-        'content': content
-    }
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-def crawl_wiki(start_url, max_depth):
-    # Keep track of visited URLs and their depth
-    visited = {start_url: 0}
-    
-    # Keep track of URLs to visit in the future
-    queue = [start_url]
-    
-    while queue:
-        # Get the next URL to visit
-        url = queue.pop(0)
-        depth = visited[url]
-        
-        # Skip URLs that exceed the maximum depth
-        if depth > max_depth:
-            continue
-        
-        # Process the current URL
-        print(f'Processing {url}')
-        data = process_wiki_page(url)
-        
-        # Do something with the processed data
-        # ...
-        
-        # Find and queue new URLs to visit
-        links = soup.find_all('a', href=True)
-        for link in links:
-            href = link['href']
-            
-            # Skip non-Wiki URLs
-            if not href.startswith('/wiki/'):
-                continue
-            
-            # Skip URLs with colons (e.g. File: links)
-            if ':' in href:
-                continue
-            
-            # Convert the relative URL to an absolute URL
-            new_url = f'https://en.wikipedia.org{href}'
-            
-            # Skip URLs that have already been visited
-            if new_url in visited:
-                continue
-            
-            # Add the new URL to the queue
-            queue.append(new_url)
-            visited[new_url] = depth + 1
+def save_data_to_file(folder, file_name, content):
+    with open(os.path.join(folder, file_name), "w", encoding="utf-8") as file:
+        file.write(content)
 
-if __name__ == '__main__':
-    start_url = 'https://en.wikipedia.org/wiki/Main_Page'
-    max_depth = 2
-    crawl_wiki(start_url, max_depth)
+def extract_data_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
+        return soup
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to fetch data from URL: {url} - {e}")
+        return None
+
+def read_links_file(file_name):
+    with open(file_name, "r") as links_file:
+        links = [link.strip() for link in links_file.readlines()]
+    return links
+
+def process_url(link, data_folder):
+    soup = extract_data_from_url(link)
+
+    if soup is None:
+        logging.warning(f"Skipping URL: {link} due to an error while fetching data")
+        return False, link
+
+    heading_element = soup.find("h1")
+
+    if heading_element is None:
+        logging.warning(f"Couldn't find heading for URL: {link}")
+        return False, link
+
+    heading = heading_element.text.strip()
+
+    invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+    for char in invalid_chars:
+        heading = heading.replace(char, '_')
+
+    first_letter = heading[0].upper()
+    letter_folder = os.path.join(data_folder, first_letter)
+    create_folder(letter_folder)
+
+    content_selector = ".mw-parser-output"
+    content_element = soup.select_one(content_selector)
+
+    if content_element is None:
+        logging.warning(f"Couldn't find main content for URL: {link}")
+        return False, link
+
+    content = content_element.text.strip()
+
+    file_name = f"{heading}.txt"
+    save_data_to_file(letter_folder, file_name, content)
+    logging.info(f"Successfully saved data from URL: {link} to {os.path.join(letter_folder, file_name)}")
+    return True, link
+
+def main():
+    data_folder = "Data"
+    create_folder(data_folder)
+
+    links = read_links_file("links.txt")
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_url, link, data_folder) for link in links]
+
+        failed_links = [future.result()[1] for future in futures if not future.result()[0]]
+
+    if failed_links:
+        logging.info("Retrying failed URLs...")
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            retry_futures = [executor.submit(process_url, link, data_folder) for link in failed_links]
+
+            retried_links = [future.result()[1] for future in retry_futures if not future.result()[0]]
+
+        if retried_links:
+            logging.warning(f"Saving {len(retried_links)} failed URLs to skip.txt")
+            with open("skip.txt", "w") as skip_file:
+                for link in retried_links:
+                    skip_file.write(f"{link}\n")
+
+if __name__ == "__main__":
+    main()
